17da4e658a0017a38ce906de080be5054ad6d074
KimYoungWook730 <wook5272@naver.com>
2026-05-06 12:34:11 +0900

    test

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,15 +1,36 @@
 import ButtonCom from "./ButtonCom/ButtonCom"
-import {handleClick} from "./handle"
+import {handlePlay, handleStop} from "./handle"
+import sampleVideo from "../assets/s.mp4"
 
-export default function Toolbar () {
+// export default function Toolbar () {
+//     return(
+//         <>
+//             <ButtonCom message="버튼1 클릭" handle={handleClick}>
+//                 버튼1
+//             </ButtonCom>
+//             <ButtonCom message="버튼2 클릭" handle={handleClick}>
+//                 버튼2
+//             </ButtonCom>
+//         </>
+//     )
+// }
+
+export default function Toolbar() {
     return(
         <>
-            <ButtonCom message="버튼1 클릭" handle={handleClick}>
-                버튼1
-            </ButtonCom>
-            <ButtonCom message="버튼2 클릭" handle={handleClick}>
-                버튼2
-            </ButtonCom>
+            <nav>
+                <ButtonCom message="videoPlayer" handle={handlePlay}>
+                    Play
+                </ButtonCom>
+                &nbsp;
+                <ButtonCom message="videoPlayer" handle={handleStop}>
+                    Stop
+                </ButtonCom>
+            </nav>
+            <br/>
+            <section>
+                <video id="videoPlayer" src={sampleVideo} controls width="350" />
+            </section>
         </>
     )
 }
